756842d58c83a4e118e8ad3e06b124bd0676f387
Santa Kesane <s.plenkova@gmail.com>
2022-11-15 15:13:52 +0200

    calculator

--- HelloWorldVS/VSintro/VSintro/Program.cs
+++ HelloWorldVS/VSintro/VSintro/Program.cs
@@ -1,13 +1,34 @@
 ﻿
 
+using System.Formats.Asn1;
+
 class VSintro
 
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
 
-
+        int intvariable;
         // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!7777777");
+        //// Console.WriteLine("Hello, World!7777777");
+        ////Console.WriteLine("Hello, World!7777777");
+        // Console.writenline(intvariable);
+        // int intvariable = 18;
+        // Console.writenline(intvariable);
+        // int intvariable;
+
+        string name = "John";
+        string string = "5555";
+        uint age = 18; /// unsigned integer, because age can`t be negative
+    
+        //int age = 18;///unsigned integer
+        char gender = 'M';
+        float height = 1.87F;
+        bool underaged;
+        
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(gender);
+        Console.WriteLine(height);
     }
  }
