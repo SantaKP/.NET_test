6ca64c276108993b393b03617bcfc75c8381c8dc
Santa Kesane <s.plenkova@gmail.com>
2022-11-22 16:40:33 +0200

    corrected files

---
+++ Tasks_21.11.22/Advanced_calculator/Advanced_calculator/Advanced_calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿class Advanced_calculator
+{
+
+
+
+
+    static void Main(String[] args)
+
+    {
+
+        Console.WriteLine("Enter your first number");
+        string number1str = Console.ReadLine();
+        Console.WriteLine("Enter your second number");
+        string number2str = Console.ReadLine();
+        int number1 = Int32.Parse(number1str);
+        int number2 = Int32.Parse(number2str);
+        Console.Write("Enter symbol(/,+,-,*):");
+        string symbol = Console.ReadLine();
+        switch (symbol)
+        {
+            case "+":
+                int addition = number1 + number2;
+                Console.WriteLine("Addition:" + addition);
+                break;
+            case "-":
+                int substraction = number1 - number2;
+                Console.WriteLine("Subtraction:" + substraction);
+                break;
+            case "*":
+                int multiplication = number1 * number2;
+                Console.WriteLine("Multiplication:" + multiplication);
+                break;
+            case "/":
+                double division = number1 / number2;
+                Console.WriteLine("Division:" + division);
+                break;
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
---
+++ calculator_task/calculator_task/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+Console.WriteLine("Enter first number:");
+string number1str = Console.ReadLine();
+Console.WriteLine("Enter second number:");
+string number2str = Console.ReadLine();
+int number1 = Int32.Parse(number1str);
+int number2 = Int32.Parse(number2str);
+int adition = number1 + number2;
+Console.WriteLine("Addition: " + adition);
+int substraction = number1 - number2;
+Console.WriteLine("Substraction: " + substraction);
+int multiplication = number1 * number2;
+Console.WriteLine("Multiplication: " + multiplication);
+double number_1 = Double.Parse(number1str);
+double number_2 = Double.Parse(number2str);
+double division = number_1 / number_2;
+Console.WriteLine("Division: " + division);
